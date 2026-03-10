cf7c312aecb483e720606f195d3362cb6d02a003
YogSotho <monnocellula@gmail.com>
2026-03-10 18:14:52 +0100

    Delete engines directory

--- engines/base.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC, abstractmethod
-from typing import List
-
-class BaseEngine(ABC):
-    @abstractmethod
-    def generate(self, prompt: str) -> str: ...
-    @abstractmethod
-    def generate_batch(self, prompts: List[str]) -> List[str]: ...
--- engines/init.py
+++
@@ -1,7 +0,0 @@
-from .base import BaseEngine
-from .teacher_engine import TeacherEngine
-from .vllm_engine import VLLMEngine
-from .ensemble_engine import EnsembleTeacher
-from .judge_engine import JudgeEngine
-
-__all__ = ["BaseEngine", "TeacherEngine", "VLLMEngine", "EnsembleTeacher", "JudgeEngine"]
