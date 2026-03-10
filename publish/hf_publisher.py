3f875abdfc49f9d1312482d47ff7a4a13c220f24
YogSotho <monnocellula@gmail.com>
2026-03-10 18:01:56 +0100

    Update hf_publisher.py

--- publish/hf_publisher.py
+++ publish/hf_publisher.py
@@ -1,12 +1,8 @@
 from huggingface_hub import HfApi
 from datasets import load_dataset
+import os
 
-
-def publish_dataset(dataset_path, repo_name):
-
-    dataset = load_dataset(
-        "json",
-        data_files=dataset_path
-    )
-
-    dataset.push_to_hub(repo_name)
+def publish_dataset(dataset_path: str, repo_name: str, token: str | None = None) -> None:
+    token = token or os.getenv("HF_TOKEN")
+    dataset = load_dataset("json", data_files=dataset_path)
+    dataset.push_to_hub(repo_name, token=token)
